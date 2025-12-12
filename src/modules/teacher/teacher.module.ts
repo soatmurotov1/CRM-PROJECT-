f76b8e0ec04458d182249e4f85cd2f94bd3bae2f
soatmurotov1 <soatmurotovabrorbek23@gmail.com>
2025-12-12 09:59:07 +0500

    bag fixed

--- src/modules/teacher/teacher.module.ts
+++ src/modules/teacher/teacher.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TeacherService } from './teacher.service';
 import { PrismaService } from 'prisma/prisma.service';
+import { TeacherController } from './teacher.controller';
 
 @Module({
+  controllers: [TeacherController],
   providers: [TeacherService, PrismaService],
   exports: [TeacherService]
 })
